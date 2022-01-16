43aaa331680ba2cf6153fd42c886d2b7ab621fb5
WMS <82312581+WMSGaming@users.noreply.github.com>
2022-01-16 11:30:14 +0000

    Update Webhook_Spammer.py

--- Webhook_Spammer.py
+++ Webhook_Spammer.py
@@ -1,20 +1,26 @@
-from dhooks import Webhook
+from dhooks import Webhook,file
 import time
 
-print("===================")
-print("WMS Webhook Spammer")
-print("===================")
+print("""
+ __      __ __  __  ___       __      __      _     _               _          ___  _ __                                     
+ \ \    / /|  \/  |/ __|      \ \    / / ___ | |__ | |_   ___  ___ | |__      / __|| '_ \ __ _  _ __   _ __   ___  _ _       
+  \ \/\/ / | |\/| |\__ \       \ \/\/ / / -_)|  _ \|   \ / _ \/ _ \| / /      \__ \| .__// _` || '  \ | '  \ / -_)| '_|      
+   \_/\_/  |_|  |_||___/        \_/\_/  \___||____/|_||_|\___/\___/|_\_\      |___/|_|   \__/_||_|_|_||_|_|_|\___||_|        
+
+""")
+
+
 
 spam = input("What would you like to spam? ")
-
+avatarurl = input("What is your avatar url?")
 webhooks = input("Enter webhook to spam:")
-count = 1
-
+count = True
 hook = Webhook(webhooks)
 
 
-while count > 0:
-    hook.send(spam)
+while count:
+    hook.send(spam,avatar_url=avatarurl)
     time.sleep(0.8)
+    print("Attacking Webhook server")
 
 
